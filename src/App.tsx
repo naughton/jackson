50228a2c45b418611b5c3dfcd38b52ffb1f839c8
Patrick Naughton <patrikjnaughton@gmail.com>
2019-10-13 20:43:16 -0700

    get rid of serviceworker update readme

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Jackson's first React App.
         </p>
         <a
           className="App-link"
