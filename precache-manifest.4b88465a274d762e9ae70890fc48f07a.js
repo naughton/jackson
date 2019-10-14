0e03e9f9e0ddb3009f94e8de56350973ed195c99
Patrick Naughton <patrickjnaughton@gmail.com>
2019-10-13 21:13:43 -0700

    Updates

---
+++ precache-manifest.4b88465a274d762e9ae70890fc48f07a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d37f210e32c262541286c91b8af7bcc",
+    "url": "/jackson/index.html"
+  },
+  {
+    "revision": "949c66d60973a187875a",
+    "url": "/jackson/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "7187467274b8c15e386e",
+    "url": "/jackson/static/js/2.a22c8588.chunk.js"
+  },
+  {
+    "revision": "949c66d60973a187875a",
+    "url": "/jackson/static/js/main.b35f3529.chunk.js"
+  },
+  {
+    "revision": "be053a285cf7c093c324",
+    "url": "/jackson/static/js/runtime-main.18bd75b1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jackson/static/media/logo.25bf045c.svg"
+  }
+]);
